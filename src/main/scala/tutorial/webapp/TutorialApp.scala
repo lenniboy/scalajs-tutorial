db693cc71ae36cae1e195636b3f6b324975546c1
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2015-08-09 17:17:58 +0200

    Move all web components into their own class

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,6 +1,7 @@
 package tutorial.webapp
 
 import org.scalajs.dom
+import org.scalajs.dom.Event
 import org.scalajs.dom.raw.HTMLElement
 
 import scala.scalajs.js.JSApp
@@ -10,26 +11,17 @@ object TutorialApp extends JSApp
   with Appending {
 
   def main(): Unit = {
-    val dropDown = new DropDown(textUpdate = println)
-    val container = new MainContainer(Seq(dropDown))
-    append(container)
+    dom.document.addEventListener("DOMContentLoaded", { e: Event =>
+      println("loaded")
+      val dropDown = new DropDown(textUpdate = println)
+      val container = new MainContainer(Seq(dropDown))
+      append(container)
+    })
   }
-
-}
-
-class MainContainer(children: Seq[WebComponent]) extends WebComponent {
-
-  private val mainContainer = div(`class` := "container")
-
-  override def render: HTMLElement = {
-    val container = mainContainer.render
-    children.foreach( c => container.appendChild(c.render))
-    container
-  }
-
 }
 
 trait Appending {
   def append(component: WebComponent): Unit = dom.document.body.appendChild(component.render)
 }
+
 
--- src/main/scala/tutorial/webapp/WebComponent.scala
+++ src/main/scala/tutorial/webapp/WebComponent.scala
@@ -1,6 +1,8 @@
 package tutorial.webapp
 
-import org.scalajs.dom.raw.HTMLElement
+import org.scalajs.dom.raw.{HTMLElement, KeyboardEvent}
+
+import scalatags.JsDom.all._
 
 trait WebComponent {
   def render: HTMLElement
@@ -8,4 +10,42 @@ trait WebComponent {
 
 object NoOps{
   val String = { s:String => () }
+}
+
+class MainContainer(children: Seq[WebComponent]) extends WebComponent {
+
+  private val mainContainer = div(`class` := "container",
+    h1("Web components with ScalaJS")
+  )
+
+  override def render: HTMLElement = {
+    val container = mainContainer.render
+    children.foreach( c => container.appendChild(c.render))
+    container
+  }
+
+}
+
+class DropDown(textUpdate: (String => Unit) = NoOps.String) extends WebComponent {
+
+  private val html = input(
+    placeholder := "Type to uppercase",
+    `class` := "form-control",
+    `type` := "text"
+  ).render
+
+  html.onkeyup = { e: KeyboardEvent =>
+    textUpdate(html.value)
+  }
+
+  override def render: HTMLElement = html
+}
+
+class TextOutput(initialText: String = "") extends WebComponent {
+
+  private val output = blockquote(
+    p(initialText)
+  )
+
+  override def render: HTMLElement = output.render
 }
