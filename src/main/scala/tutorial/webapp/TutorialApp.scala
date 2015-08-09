f35f0a74ee72e1db2a245eb0e444893a3b37eb71
Leonard Ehrenfried <leonard.ehrenfried@gmail.com>
2015-08-09 16:33:39 +0200

    Add default noops

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -19,11 +19,12 @@ object TutorialApp extends JSApp
   def setupUI(): Unit = {
     jQuery("""<button type="button">Click me!</button>""")
       .appendTo(jQuery("body"))
+      .click(addClickedMessage)
     jQuery("body").append("<p>World</p>")
   }
 
   def addClickedMessage(): Unit = {
-    val dropDown = new DropDown(textUpdate = Some(println))
+    val dropDown = new DropDown(textUpdate = println)
     append(dropDown)
   }
 
@@ -38,21 +39,22 @@ trait Appending {
 
 }
 
+object NoOps{
+  val String = { s:String => () }
+}
+
 trait WebComponent {
   def render: HTMLElement
 }
 
-class DropDown(textUpdate: Option[(String => Unit)] = None) extends WebComponent {
+class DropDown(textUpdate: (String => Unit) = NoOps.String) extends WebComponent {
 
   private val html = input(
     placeholder := "Type to uppercase"
   ).render
 
   html.onkeyup = { e: KeyboardEvent =>
-    textUpdate.foreach { f =>
-      val value = html.value
-      f(value)
-    }
+    textUpdate(html.value)
   }
 
   override def render: HTMLElement = html
